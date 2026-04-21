483a59c5828eaa9be0ad046113291aa6fd21511c
samuelamorimm <samuelhenriqueamorim@gmail.com>
2026-04-21 00:51:40 -0300

    feat: adiciona parse_minutes para converter tempo offline em minutos

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import re
 st.set_page_config(page_title="APP Eace", layout="centered")
 st.title("Agrupador de Massivas")
 
+
+
 lista_sites = st.text_area("Cole a lista de sites DONW aqui:", height=200)
 
 #percorrer blocos de sites filtrando infos
@@ -22,15 +24,39 @@ def parse_sites(texto):
         municipio = re.search(r'\[SITE DOWN\] [A-Z]{2} (.+?) \(', bloco)
         provedor = re.search(r'Provedor: (.+?)\n', bloco)
         tempo_off = re.search(r'Tempo Parado: (.+?)\n', bloco)
-
+        
         site['inep'] = inep_m.group(1) if inep_m else None
         site['uf'] = uf_m.group(1) if uf_m else None
         site['municipio'] = municipio.group(1) if municipio else None
         site['provedor'] = provedor.group(1) if provedor else None
         site['tempo_off'] = tempo_off.group(1) if tempo_off else None
+
+        tempo_off_min = parse_minutes(site['tempo_off'])
+        site['tempo_off_min'] = tempo_off_min
+
         sites.append(site)
     
     return sites
+
+#função que converte o tempo para minutos
+def parse_minutes(texto):
+    if not texto:
+        return None
+    
+    #dias
+    d = re.search(r'(\d+)d', texto)
+    d = int(d.group(1)) if d else 0
+
+    #horas
+    h = re.search(r'(\d+)h', texto)
+    h = int(h.group(1)) if h else 0
+
+    #minutos
+    m = re.search(r'(\d+)m', texto)
+    m = int(m.group(1)) if m else 0
+
+    min = d * 1440 + h * 60 + m
+    return min
 
 
 #processamento de massivas
